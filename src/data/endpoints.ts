839f0abc5d895a16b777b79a64d029edbe6eddc7
developer-jawad <jawadahmad.developer@gmail.com>
2024-11-21 04:48:24 +0500

    fix: update backend url

--- src/data/endpoints.ts
+++ src/data/endpoints.ts
@@ -1,5 +1,5 @@
-// const baseURL = "http://35.170.248.57:8000/"
-const baseURL = "http://127.0.0.1:8000/"
+const baseURL = "http://35.170.248.57:8000/"
+// const baseURL = "http://127.0.0.1:8000/"
 // const baseURL = "http://localhost:8000/"
 
 export const endPoints = {
