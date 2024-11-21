304ca3ae5f0975df327780541850d281d4b3191f
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2024-11-22 02:16:15 +0500

    A

--- src/data/endpoints.ts
+++ src/data/endpoints.ts
@@ -1,4 +1,4 @@
-const baseURL = "http://35.170.248.57:8000/"
+const baseURL = "http://54.89.176.129:8000/"
 // const baseURL = "http://127.0.0.1:8000/"
 // const baseURL = "http://localhost:8000/"
 
