45c8d3a5d479a079fadefea80c31981666f40546
penamorrosmaaaa <110785872+penamorrosmaaaa@users.noreply.github.com>
2025-02-17 10:51:51 -0600

    Update GeneralApp.tsx

--- src/Pages/GeneralApp.tsx
+++ src/Pages/GeneralApp.tsx
@@ -22,157 +22,148 @@ import AmpOverview from "./AmpOverview";
 import { TestManager } from "./TestManager";
 import ImageData from "./ImageData";
 
-
 const GeneralApp: React.FC = () => {
-    const [activeTab, setActiveTab] = useState(0);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  
-   
-    
-    return (
-      <div className="app-container flex">
-        <div className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
-          <div className="sidebar-header">
-            <div className="logo-container">
-              <img src="/logo.png" alt="Logo" className="logo" />
-            </div>
-            <h2 className="title">Digital</h2>
-            <h2 className="title">Benchmarks</h2>
-            <hr className="separator" />
-          </div>
-  
-          <button
-            className={`tab-button ${activeTab === 0 ? "active" : ""}`}
-            onClick={() => setActiveTab(0)}
-          >
-            <img
-              src={General}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            General Overview
-          </button>
-  
-          <button
-            className={`tab-button ${activeTab === 1 ? "active" : ""}`}
-            onClick={() => setActiveTab(1)}
-          >
-            <img
-              src={Vertical}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            Vertical Overview
-          </button>
-  
-          <button
-            className={`tab-button ${activeTab === 2 ? "active" : ""}`}
-            onClick={() => setActiveTab(2)}
-          >
-            <img
-              src={Local}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            Local Overview
-          </button>
+  const [activeTab, setActiveTab] = useState(0);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-          <button
-            className={`tab-button ${activeTab === 3 ? "active" : ""}`}
-            onClick={() => setActiveTab(3)}
-          >
-            <img
-              src={ampIcon}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            AMP Overview
-          </button>
-          <button
-            className={`tab-button ${activeTab === 4 ? "active" : ""}`}
-            onClick={() => setActiveTab(4)}
-          >
-            <img
-              src={testIcon}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            Test Manager
-          </button>
-          <button
-            className={`tab-button ${activeTab === 5 ? "active" : ""}`}
-            onClick={() => setActiveTab(5)}
-          >
-            <img
-              src={ImageIcon}
-              height={10}
-              width={20}
-              style={{
-                marginRight: "10px",
-              }}
-            />{" "}
-            Gallery
-          </button>
+  return (
+    <div className="app-container flex" style={{ color: "black" }}>
+      <div className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
+        <div className="sidebar-header">
+          <div className="logo-container">
+            <img src="/logo.png" alt="Logo" className="logo" />
+          </div>
+          <h2 className="title">Digital</h2>
+          <h2 className="title">Benchmarks</h2>
+          <hr className="separator" />
         </div>
-  
+
         <button
-          className="toggle-sidebar"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          style={{
-            border: "none",
-            outline: "none",
-          }}
+          className={`tab-button ${activeTab === 0 ? "active" : ""}`}
+          onClick={() => setActiveTab(0)}
         >
-          {isSidebarOpen ? (
-            <FontAwesomeIcon icon={faChevronLeft} />
-          ) : (
-            <FontAwesomeIcon icon={faChevronRight} />
-          )}
+          <img
+            src={General}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="General Icon"
+          />{" "}
+          General Overview
         </button>
-  
-        {/* <button
-          className="toggle-sidebar"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          style={{
-            border:"none",
-            outline:"none",
-          }}
+
+        <button
+          className={`tab-button ${activeTab === 1 ? "active" : ""}`}
+          onClick={() => setActiveTab(1)}
         >
-          {isSidebarOpen ? <AiOutlineArrowLeft /> : <AiOutlineArrowRight />}
-        </button> */}
-  
-        <div className="content">
-          <section
-            className={`ContainerSetting ${
-              isSidebarOpen ? "sidebar-open" : "sidebar-closed"
-            }`}
-          >
-            {activeTab === 0 && <GeneralOverview />}
-            {activeTab === 1 && <VerticalOverview />}
-            {activeTab === 2 && <LocalOverview />}
-            {activeTab === 3 && <AmpOverview />}
-            {activeTab === 4 && <TestManager />}
-            {activeTab === 5 && <ImageData />}
-          </section>
-        </div>
+          <img
+            src={Vertical}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="Vertical Icon"
+          />{" "}
+          Vertical Overview
+        </button>
+
+        <button
+          className={`tab-button ${activeTab === 2 ? "active" : ""}`}
+          onClick={() => setActiveTab(2)}
+        >
+          <img
+            src={Local}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="Local Icon"
+          />{" "}
+          Local Overview
+        </button>
+
+        <button
+          className={`tab-button ${activeTab === 3 ? "active" : ""}`}
+          onClick={() => setActiveTab(3)}
+        >
+          <img
+            src={ampIcon}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="AMP Icon"
+          />{" "}
+          AMP Overview
+        </button>
+        <button
+          className={`tab-button ${activeTab === 4 ? "active" : ""}`}
+          onClick={() => setActiveTab(4)}
+        >
+          <img
+            src={testIcon}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="Test Manager Icon"
+          />{" "}
+          Test Manager
+        </button>
+        <button
+          className={`tab-button ${activeTab === 5 ? "active" : ""}`}
+          onClick={() => setActiveTab(5)}
+        >
+          <img
+            src={ImageIcon}
+            height={10}
+            width={20}
+            style={{
+              marginRight: "10px",
+            }}
+            alt="Gallery Icon"
+          />{" "}
+          Gallery
+        </button>
       </div>
-    );
-  };
-  
-  export default GeneralApp;
-  
+
+      <button
+        className="toggle-sidebar"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        style={{
+          border: "none",
+          outline: "none",
+        }}
+      >
+        {isSidebarOpen ? (
+          <FontAwesomeIcon icon={faChevronLeft} />
+        ) : (
+          <FontAwesomeIcon icon={faChevronRight} />
+        )}
+      </button>
+
+      <div className="content">
+        <section
+          className={`ContainerSetting ${
+            isSidebarOpen ? "sidebar-open" : "sidebar-closed"
+          }`}
+        >
+          {activeTab === 0 && <GeneralOverview />}
+          {activeTab === 1 && <VerticalOverview />}
+          {activeTab === 2 && <LocalOverview />}
+          {activeTab === 3 && <AmpOverview />}
+          {activeTab === 4 && <TestManager />}
+          {activeTab === 5 && <ImageData />}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default GeneralApp;
