02daabe684ce3344aeb970a918bbecd743bd1c8b
penamorrosmaaaa <110785872+penamorrosmaaaa@users.noreply.github.com>
2025-02-17 11:02:17 -0600

    Update theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,7 +5,7 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
-  initialColorMode: "dark",
+  initialColorMode: "white",
   useSystemColorMode: false,
 };
 
