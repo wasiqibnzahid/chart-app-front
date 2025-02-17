dfca65db6c7b3e3f525807308633f0bbe6f8b8c6
penamorrosmaaaa <110785872+penamorrosmaaaa@users.noreply.github.com>
2025-02-17 12:39:10 -0600

    Update DateDisplay.jsx

--- src/components/common/DateDisplay.jsx
+++ src/components/common/DateDisplay.jsx
@@ -1,25 +1,37 @@
 import { parseAndFormatDate } from '/src/utils/utils.js';
 
-export default function DateDisplay({currentRecordIndex, setCurrentRecordIndex, totalLength, date}) {
-    const previousIndex = currentRecordIndex - 1 > 0? currentRecordIndex - 1: 0;
-    const nextIndex = currentRecordIndex + 1 >= totalLength? currentRecordIndex: currentRecordIndex + 1;
-    console.log(nextIndex, totalLength, currentRecordIndex)
-    return(
-        <div className="d-flex top-row text-white custom-row justify-content-start" style={{background: `linear-gradient(45deg, rgb(0, 0, 0), rgb(73 21 132 / 0%))`, padding: '12px 24px', boxShadow: '0 0 4px rgb(73 21 132 / 50%))'}}>
-            {
-                previousIndex ?
-                    <span onClick={() => setCurrentRecordIndex(previousIndex)}><img width={24} src="/src/assets/arrow-left.svg" alt="left arrow" /></span>
-                    : null
-                
-            }
-            <div className="mx-3" style={{width: 'max-content'}}>
-               <strong> Viewing data of {parseAndFormatDate(date || '')}</strong>
+export default function DateDisplay({ currentRecordIndex, setCurrentRecordIndex, totalLength, date }) {
+    const previousIndex = currentRecordIndex - 1 > 0 ? currentRecordIndex - 1 : 0;
+    const nextIndex = currentRecordIndex + 1 >= totalLength ? currentRecordIndex : currentRecordIndex + 1;
+    console.log(nextIndex, totalLength, currentRecordIndex);
+    return (
+        <div 
+            className="d-flex top-row custom-row justify-content-start" 
+            style={{ background: "none", padding: '12px 24px', color: "black" }}
+        >
+            {previousIndex ? (
+                <span onClick={() => setCurrentRecordIndex(previousIndex)}>
+                    <img 
+                        width={24} 
+                        src="/src/assets/arrow-left.svg" 
+                        alt="left arrow" 
+                        style={{ filter: "brightness(0)" }} 
+                    />
+                </span>
+            ) : null}
+            <div className="mx-3" style={{ width: 'max-content' }}>
+               <strong>Viewing data of {parseAndFormatDate(date || '')}</strong>
             </div>
-            {
-                !(nextIndex == currentRecordIndex) ?
-                    <span className={'disabled'} onClick={() => setCurrentRecordIndex(nextIndex)}><img width={24} src="/src/assets/arrow-right.svg" alt="right arrow" /></span>
-                    : null
-            }
+            {!(nextIndex === currentRecordIndex) ? (
+                <span className="disabled" onClick={() => setCurrentRecordIndex(nextIndex)}>
+                    <img 
+                        width={24} 
+                        src="/src/assets/arrow-right.svg" 
+                        alt="right arrow" 
+                        style={{ filter: "brightness(0)" }} 
+                    />
+                </span>
+            ) : null}
         </div>
     );
 }
