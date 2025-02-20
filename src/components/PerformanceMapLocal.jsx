ef5c2d6b93359d96da2477428986a05077650441
penamorrosmaaaa <110785872+penamorrosmaaaa@users.noreply.github.com>
2025-02-20 10:32:23 -0600

    Update PerformanceMapLocal.jsx

--- src/components/PerformanceMapLocal.jsx
+++ src/components/PerformanceMapLocal.jsx
@@ -355,7 +355,7 @@ const PerformanceMapLocal = () => {
           y0: 0,
           y1: 1,
           line: {
-            color: "black",
+            color: "turquoise",
             width: 2,
             dash: "dot",
           },
