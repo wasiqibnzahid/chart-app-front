9a3fc0eb13bcd7d8521d03ec974a9868a3b995bb
penamorrosmaaaa <110785872+penamorrosmaaaa@users.noreply.github.com>
2025-02-24 13:44:50 -0600

    Update comparison-chart-ungroup.tsx

--- src/components/comparison-chart-ungroup.tsx
+++ src/components/comparison-chart-ungroup.tsx
@@ -487,7 +487,7 @@ const ComparisonNoGroup: React.FC<BarChartProps> = ({
                 value={selectedOption}
                 onChange={(e) => setSelectedOption(e.target.value)}
                 border="2px"
-                borderColor="#cbd5e0" // Apply the border color
+                borderColor="#black" // Apply the border color
                 borderRadius="8px"
                 size="sm"
                 color="black"
@@ -588,7 +588,7 @@ const ComparisonNoGroup: React.FC<BarChartProps> = ({
             <span
               key={month}
               style={{
-                color: "#cbd5e0",
+                color: "#black",
               }}
             >
               {month}
